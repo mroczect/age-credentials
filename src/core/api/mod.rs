fc598276c3591c6ccb906aeae4753c6a3205957e
mroczect <mroczect@proton.me>
2026-08-03 23:34:39 +0700

    feat(api): re-export submodules

--- src/core/api/mod.rs
+++ src/core/api/mod.rs
@@ -2,3 +2,8 @@ pub mod private_keys;
 pub mod public_keys;
 pub mod user_email;
 pub mod user_name;
+
+pub use private_keys::*;
+pub use public_keys::*;
+pub use user_email::*;
+pub use user_name::*;
