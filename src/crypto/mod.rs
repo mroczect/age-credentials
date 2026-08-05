229513858e48ec248d67e66dc9d8ba64a0f5ffbc
mroczect <mroczect@proton.me>
2026-08-05 21:40:03 +0700

    feat(crypto): add mod

---
+++ src/crypto/mod.rs
@@ -0,0 +1,15 @@
+pub mod armor;
+pub mod ascii;
+pub mod decrypt;
+pub mod encrypt;
+pub mod keygen;
+pub mod passphrase;
+pub mod recipient;
+
+pub use armor::*;
+pub use ascii::*;
+pub use decrypt::*;
+pub use encrypt::*;
+pub use keygen::*;
+pub use passphrase::*;
+pub use recipient::*;
